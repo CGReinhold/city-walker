044eb62363dbaf93886dde1bb22710711e7e8850
Cleyson Reinhold <cleyson.reinhold@gmail.com>
2021-03-22 17:20:25 -0300

    first commit

---
+++ src/cities.js
@@ -0,0 +1,569 @@
+export const CITIES = [
+  {
+    name: 'Abu Dhabi, United Arab Emirates',
+    videos: {
+      sunny: '2vmCBBPsnrU',
+    }
+  },
+  {
+    name: 'Amsterdam, Netherlands',
+    videos: {
+      sunny: 'E07rTPgIvn0',
+      rainy: 'N0af7_w5GBQ',
+      night: 'RT72BhusO_I',  
+    }
+  },
+  {
+    name: 'Antalya, Turkey',
+    videos: {
+      sunny: 'OtmKmiMpuxQ',
+    }
+  },
+  {
+    name: 'Athens, Greece',
+    videos: {
+      sunny: 'KBgfbxgD22U',
+      rainy: 'e5JqyDJSD0k',
+      night: 'XnQSWZdLcEA',
+    }
+  },
+  {
+    name: 'Barcelona, Spain',
+    videos: {
+      sunny: '-ZXjFeBd9Us',
+      rainy: 'ZFx09blLVJA',
+      night: 'w5AzkqYyiJE',
+    }
+  },
+  {
+    name: 'Bangkok, Thailand',
+    videos: {
+      sunny: 'DxP8fc2XYb4',
+      rainy: 'AddoY2kFKcU',
+      night: 'exjJSEIC_YQ',
+    }
+  },
+  {
+    name: 'Beijing, China',
+    videos: {
+      sunny: 'MgqkUDOpTJQ',
+    }
+  },
+  {
+    name: 'Beirut, Lebanon',
+    videos: {
+      sunny: 'KMDWEwQDoc0',
+    }
+  },
+  {
+    name: 'Belfast, Northern Ireland',
+    videos: {
+      sunny: 'chb0suSrNxg',
+    }
+  },
+  {
+    name: 'Belgrade, Serbia',
+    videos: {
+      sunny: 'WmKNcI4atpA',
+    }
+  },
+  {
+    name: 'Berlin, Germany',
+    videos: {
+      sunny: 'zca688Ecwpk',
+      night: 'uZvOQx1rvDU',
+      rainy: 'CAGwCvlogY8',
+    }
+  },
+  {
+    name: 'Bern, Switzerland',
+    videos: {
+      sunny: 'aTDbxGyz07k',
+      rainy: 'fThtIUufqcQ',
+    }
+  },
+  {
+    name: 'Bogota, Colombia',
+    videos: {
+      sunny: 'XiOLYmF70fA',
+    }
+  },
+  {
+    name: 'Brasilia, Brazil',
+    videos: {
+      sunny: '8nPL9YBWV0o',
+    }
+  },
+  {
+    name: 'Bratislava, Slovakia',
+    videos: {
+      sunny: 'gMllzBuAbBg',
+    }
+  },
+  {
+    name: 'Brussels, Belgium',
+    videos: {
+      sunny: 'DmmEKbLOzTg',
+    }
+  },
+  {
+    name: 'Bucharest, Romania',
+    videos: {
+      sunny: 'pa4n-_T1FOE',
+    }
+  },
+  {
+    name: 'Budapest, Hungary',
+    videos: {
+      sunny: 'ZzvG-JKjvto',
+      night: 'QLXnAh_J9Bk',
+    }
+  },
+  {
+    name: 'Buenos Aires, Argentina',
+    videos: {
+      sunny: '0lrcjmhcYRw',
+      night: 'R3QQr6qVOn0',
+    }
+  },
+  {
+    name: 'Cairo, Egypt',
+    videos: {
+      sunny: 'YVYDz_coSfY',
+    }
+  },
+  {
+    name: 'Chicago, USA',
+    videos: {
+      sunny: '5alwy_vLWUM',
+      night: '8rOpUN6gvtE',
+    }
+  },
+  {
+    name: 'Copenhagen, Denmark',
+    videos: {
+      sunny: 'Nl69sN5PtgM',
+    }
+  },
+  {
+    name: 'Curitiba, Brazil',
+    videos: {
+      sunny: '1w5eAshOono',
+    }
+  },
+  {
+    name: 'Cusco, Peru',
+    videos: {
+      sunny: 'XuFpQeFzCXA',
+    }
+  },
+  {
+    name: 'Delhi, India',
+    videos: {
+      sunny: 'XZVBoSteNYA',
+    }
+  },
+  {
+    name: 'Dublin, Ireland',
+    videos: {
+      sunny: 'kFZQT7obIeU',
+      night: 'HhZQonD3VS8',
+    }
+  },
+  {
+    name: 'Dunedin, New Zealand',
+    videos: {
+      sunny: 'LHP3vxVgvI0',
+    }
+  },
+  {
+    name: 'Edinburgh, Scotland',
+    videos: {
+      sunny: 'MXwBUPf0YyM',
+      night: 'Bpl82J52ShI',
+    }
+  },
+  {
+    name: 'Guadalajara, Mexico',
+    videos: {
+      sunny: 'RqKtcCuGmX0',
+    }
+  },
+  {
+    name: 'Hamburg, Germany',
+    videos: {
+      sunny: 'dLmeq6hU-5E',
+    }
+  },
+  {
+    name: 'Hanoi, Vietnam',
+    videos: {
+      sunny: 'NQp9Ig9MpcM',
+    }
+  },
+  {
+    name: 'Hawaii, USA',
+    videos: {
+      sunny: 'FUcTGx-pfvI',
+    }
+  },
+  {
+    name: 'Havana, Cuba',
+    videos: {
+      sunny: '63ARQhtaUPs',
+      night: 'eym0KJ4ulq0',
+    }
+  },
+  {
+    name: 'Helsinki, Finland',
+    videos: {
+      sunny: 'y9NZL6CmUyc',
+      rainy: 'ai5waIHio1Y',
+    }
+  },
+  {
+    name: 'Istanbul, Turkey',
+    videos: {
+      sunny: 'IQH9krST07Y',
+    }
+  },
+  {
+    name: 'Izmir, Turkey',
+    videos: {
+      sunny: 'uheHx6BnBJ0',
+    }
+  },
+  {
+    name: 'Jerusalem, Israel',
+    videos: {
+      sunny: 'T96LyBlSRM4',
+    }
+  },
+  {
+    name: 'Johannesburg, South Africa',
+    videos: {
+      sunny: 'BQqoWgZ1yGk',
+    }
+  },
+  {
+    name: 'Kuala Lumpur, Malaysia',
+    videos: {
+      sunny: 'yLTycPjm0nk',
+      night: 'ftQ1wIh9bms',
+    }
+  },
+  {
+    name: 'Kyiv, Ukraine',
+    videos: {
+      sunny: '3PAFFM1m4_o',
+    }
+  },
+  {
+    name: 'La Paz, Bolivia',
+    videos: {
+      sunny: 'kjjSXN22a-g',
+    }
+  },
+  {
+    name: 'Lauterbrunnen, Switzerland',
+    videos: {
+      sunny: 'GI3IH6KJjFY',
+    }
+  },
+  {
+    name: 'Lima, Peru',
+    videos: {
+      sunny: 'CJYbl6BwpsU',
+    }
+  },
+  {
+    name: 'Lisbon, Portugal',
+    videos: {
+      sunny: 'MXlFDpaQ1ec',
+    }
+  },
+  {
+    name: 'Ljubljana, Slovenia',
+    videos: {
+      sunny: 'KtWIBwMmABg',
+      rainy: 'Kz1Mo4VTjF8',
+    }
+  },
+  {
+    name: 'London, England',
+    videos: {
+      sunny: 'MGMqLGvtNpY',
+      rainy: '__Eo-dvEH7g',
+      night: 'OYM473GeO_8',
+    }
+  },
+  {
+    name: 'Los Angeles, USA',
+    videos: {
+      sunny: 'Fhg8tvyJbAo',
+      night: 'bWpYvbznsrY',
+    }
+  },
+  {
+    name: 'Luxembourg, Luxembourg',
+    videos: {
+      sunny: 'giHQmVbOuYo',
+    }
+  },
+  {
+    name: 'Madrid, Spain',
+    videos: {
+      sunny: '2RakvEQhTTA',
+    }
+  },
+  {
+    name: 'Melbourne, Australia',
+    videos: {
+      sunny: 's0HGEcMUVtg',
+    }
+  },
+  {
+    name: 'Mexico City, Mexico',
+    videos: {
+      sunny: 'Z1SbLIKCrAo',
+    }
+  },
+  {
+    name: 'Miami, USA',
+    videos: {
+      sunny: 'sU_4nLQzhn4',
+      night: '9ylaVNd0sk0',
+    }
+  },
+  {
+    name: 'Montevideo, Uruguay',
+    videos: {
+      sunny: 'Y5dHu3vY2Uk',
+    }
+  },
+  {
+    name: 'Mumbai, India',
+    videos: {
+      sunny: 'kAzJPlXz0HE',
+    }
+  },
+  {
+    name: 'Munich, Germany',
+    videos: {
+      sunny: 'mlS89Cd176M',
+      rainy: '4lOzYUlhJEw',
+    }
+  },
+  {
+    name: 'Moscow, Russia',
+    videos: {
+      sunny: 'wi_3WwCvzo8',
+    }
+  },
+  {
+    name: 'Nairobi, Kenya',
+    videos: {
+      sunny: '_A_ANaxZpII',
+    }
+  },
+  {
+    name: 'New Delhi, India',
+    videos: {
+      sunny: '9puDiItvJKE',
+    }
+  },
+  {
+    name: 'New York City, USA',
+    videos: {
+      sunny: '-IpXdtWfneI',
+      rainy: 'eZe4Q_58UTU',
+      night: 'PEhKhACVfaA',
+    }
+  },
+  {
+    name: 'Nice, France',
+    videos: {
+      sunny: '5KWEjn839lc',
+    }
+  },
+  {
+    name: 'Oslo, Norway',
+    videos: {
+      sunny: 'f-GlXgqzMsU',
+      night: '2zMXZoFg4bc'
+    }
+  },
+  {
+    name: 'Ottawa, Canada',
+    videos: {
+      sunny: 'iAVSzMyGbO4',
+    }
+  },
+  {
+    name: 'Paris, France',
+    videos: {
+      sunny: 'Y5TNhAMEm5g',
+      night: 'Ksya1BJtu4E',
+    }
+  },
+  {
+    name: 'Porto, Portugal',
+    videos: {
+      sunny: 'q8L2edie1v0',
+    }
+  },
+  {
+    name: 'Prague, Czech Republic',
+    videos: {
+      sunny: 'tNJTWOUH8JE',
+    }
+  },
+  {
+    name: 'Reykjavik, Iceland',
+    videos: {
+      sunny: 'wW1TM6WVPJM',
+    }
+  },
+  {
+    name: 'Rio De Janeiro, Brazil',
+    videos: {
+      sunny: 'rZwXrjFFWcw',
+    }
+  },
+  {
+    name: 'Rome, Italy',
+    videos: {
+      sunny: 'EsFheWkimsU',
+      night: 'Gy6fDx9L-zk',
+      rainy: 'mmMm14CdMB4',
+    }
+  },
+  {
+    name: 'Saloniki, Greece',
+    videos: {
+      sunny: 'p3-swD6ZQts',
+    }
+  },
+  {
+    name: 'San Francisco, USA',
+    videos: {
+      sunny: 'hXCzAAYl6zo',
+    }
+  },
+  {
+    name: 'Santiago, Chile',
+    videos: {
+      sunny: 'SWEErrN6vAA',
+    }
+  },
+  {
+    name: 'Sarajevo, Bosnia and Herzegovina',
+    videos: {
+      sunny: 'ZjSecFsLUaM',
+    }
+  },
+  {
+    name: 'Sao Paulo, Brazil',
+    videos: {
+      sunny: 'xCT7VWU3I9U',
+    }
+  },
+  {
+    name: 'Seattle, USA',
+    videos: {
+      sunny: '3EaCj-seAHM',
+    }
+  },
+  {
+    name: 'Seoul, South Korea',
+    videos: {
+      sunny: '0xzDG8NLCxI',
+      night: '558lTF-lZoM',
+      rainy: '0UlOohkMZnI',
+    }
+  },
+  {
+    name: 'Singapore, Singapore',
+    videos: {
+      sunny: 'uZocEYAyUSs',
+      night: 'sngNOehNZiY',
+    }
+  },
+  {
+    name: 'Sofia, Bulgaria',
+    videos: {
+      sunny: 'ez1KCblQ6iM',
+    }
+  },
+  {
+    name: 'St. Persburg, Russia',
+    videos: {
+      sunny: 'unSFkZQiqDs',
+    }
+  },
+  {
+    name: 'Stockholm, Sweden',
+    videos: {
+      sunny: 'Jr9x-RB4E1U',
+    }
+  },
+  {
+    name: 'Taipei, Taiwan',
+    videos: {
+      sunny: '43OGN5GDqdI',
+      night: 'eIUTlDEKJXA',
+    }
+  },
+  {
+    name: 'Tel Aviv, Israel',
+    videos: {
+      sunny: 'co6L9jw_Wno',
+    }
+  },
+  {
+    name: 'Tokyo, Japan',
+    videos: {
+      sunny: 'pbiDB3N6X5E',
+      night: '0nTO4zSEpOs',
+      rainy: 'Et7O5-CzJZg'
+    }
+  },
+  {
+    name: 'Toronto, Canada',
+    videos: {
+      sunny: '5ouq5W4Or9s',
+      night: 'pZcKpivKu5Y',
+    }
+  },
+  {
+    name: 'Warsaw, Poland',
+    videos: {
+      sunny: 'UKyT_a-raW4',
+    }
+  },
+  {
+    name: 'Wuhan, China',
+    videos: {
+      sunny: 'wYu4LfqXkX4',
+    }
+  },
+  {
+    name: 'Yekaterinburg, Russia',
+    videos: {
+      sunny: 'GdzbjF-MLis',
+    }
+  },
+  {
+    name: 'Zagreb, Croatia',
+    videos: {
+      sunny: 'dUEbo0MGfM0',
+    }
+  },
+  {
+    name: 'Zurich, Switzerland',
+    videos: {
+      sunny: 'fGxbtg1ytJo',
+      night: 'jw2MwIaWx8Q',
+    }
+  },
+];
